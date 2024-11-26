acc118bf4cfdf3dc793ad69d258f815d3924edf3
simonelmundo <145836062+simonelmundo@users.noreply.github.com>
2024-11-26 18:59:18 +0100

    Update download_model.py

--- utils/sd_model_download/download_model.py
+++ utils/sd_model_download/download_model.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 model_storage_dir = os.environ['MODEL_DIR']
 hf_token = os.environ.get('HF_TOKEN', None)
-civitai_token = bf5a73346bccc8ab11cd99e1386a0e1b
+civitai_token = "bf5a73346bccc8ab11cd99e1386a0e1b"
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 
 def is_url(url_str):
