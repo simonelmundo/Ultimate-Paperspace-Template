7c8fdced5ddd3e0626ab42a710111689d562c3e4
simonelmundo <145836062+simonelmundo@users.noreply.github.com>
2024-12-08 10:35:11 +0100

    Update main.sh
    correct mistake

--- utils/sd_model_download/main.sh
+++ utils/sd_model_download/main.sh
@@ -2,93 +2,36 @@
 set -e
 
 current_dir=$(dirname "$(realpath "$0")")
-cd $current_dir
-source .env
+source $current_dir/../helper.sh
 
 # Set up a trap to call the error_exit function on ERR signal
 trap 'error_exit "### ERROR ###"' ERR
 
-
-echo "### Setting up Stable Diffusion Comfy ###"
-log "Setting up Stable Diffusion Comfy"
-if [[ "$REINSTALL_SD_COMFY" || ! -f "/tmp/sd_comfy.prepared" ]]; then
-
-    
-    TARGET_REPO_URL="https://github.com/comfyanonymous/ComfyUI.git" \
-    TARGET_REPO_DIR=$REPO_DIR \
-    UPDATE_REPO=$SD_COMFY_UPDATE_REPO \
-    UPDATE_REPO_COMMIT=$SD_COMFY_UPDATE_REPO_COMMIT \
-    prepare_repo 
-
-    symlinks=(
-      "$REPO_DIR/output:$IMAGE_OUTPUTS_DIR/stable-diffusion-comfy"
-      "$MODEL_DIR:$WORKING_DIR/models"
-      "$MODEL_DIR/sd:$LINK_MODEL_TO"
-      "$MODEL_DIR/lora:$LINK_LORA_TO"
-      "$MODEL_DIR/vae:$LINK_VAE_TO"
-      "$MODEL_DIR/upscaler:$LINK_UPSCALER_TO"
-      "$MODEL_DIR/controlnet:$LINK_CONTROLNET_TO"
-      "$MODEL_DIR/embedding:$LINK_EMBEDDING_TO"
-      "$MODEL_DIR/llm_checkpoints:$WORKING_DIR/models/LLM_checkpoints"
-    )
-    prepare_link "${symlinks[@]}"
-    rm -rf $VENV_DIR/sd_comfy-env
-    
-    
-    python3.10 -m venv $VENV_DIR/sd_comfy-env
-    
-    source $VENV_DIR/sd_comfy-env/bin/activate
-
-    pip install pip==24.0
-    pip install --upgrade wheel setuptools
-    
-    cd $REPO_DIR
-    pip install xformers
-    pip install torchvision torchaudio --no-deps
-    pip install -r requirements.txt
-    
-    touch /tmp/sd_comfy.prepared
-else
-    
-    source $VENV_DIR/sd_comfy-env/bin/activate
-    
-fi
-log "Finished Preparing Environment for Stable Diffusion Comfy"
-
-
-if [[ -z "$SKIP_MODEL_DOWNLOAD" ]]; then
-  echo "### Downloading Model for Stable Diffusion Comfy ###"
-  log "Downloading Model for Stable Diffusion Comfy"
-  bash $current_dir/../utils/sd_model_download/main.sh
-  log "Finished Downloading Models for Stable Diffusion Comfy"
-else
-  log "Skipping Model Download for Stable Diffusion Comfy"
+echo "### Setting up Model Download ###"
+if ! dpkg -s aria2 >/dev/null 2>&1; then
+    apt-get install -qq aria2 -y > /dev/null
 fi
 
+MODULES=("requests" "gdown" "bs4" "python-dotenv")
+# Loop through the modules and check if they are installed
+for module in "${MODULES[@]}"; do
+    if ! pip show $module >/dev/null 2>&1; then
+        # Module is not installed, install it with pip
+        echo "Module $module is not installed. Installing it now..."
+        pip install $module
+    fi
+done
 
-
-
-if [[ -z "$INSTALL_ONLY" ]]; then
-  echo "### Starting Stable Diffusion Comfy ###"
-  log "Starting Stable Diffusion Comfy"
-  cd "$REPO_DIR"
-  PYTHONUNBUFFERED=1 service_loop "python main.py --dont-print-server --highvram --port $SD_COMFY_PORT ${EXTRA_SD_COMFY_ARGS}" > $LOG_DIR/sd_comfy.log 2>&1 &
-  echo $! > /tmp/sd_comfy.pid
+if ! [ -v "MODEL_DIR" ]; then
+    source $current_dir/../../.env
+    export MODEL_DIR="$DATA_DIR/stable-diffusion-models"
+fi
+# This only happen when directly using this script
+if ! [ -v "MODEL_LIST" ]; then
+    env | grep -v '^_' | sed 's/\([^=]*\)=\(.*\)/\1='\''\2'\''/' > $current_dir/.env
 fi
 
+echo "### Downloading Models ###"
 
-send_to_discord "Stable Diffusion Comfy Started"
-
-if env | grep -q "PAPERSPACE"; then
-  send_to_discord "Link: https://$PAPERSPACE_FQDN/sd-comfy/"
-fi
-
-
-if [[ -n "${CF_TOKEN}" ]]; then
-  if [[ "$RUN_SCRIPT" != *"sd_comfy"* ]]; then
-    export RUN_SCRIPT="$RUN_SCRIPT,sd_comfy"
-  fi
-  bash $current_dir/../cloudflare_reload.sh
-fi
-
-echo "### Done ###"
+python $current_dir/download_model.py
+echo "### Finished Model Download ###"
